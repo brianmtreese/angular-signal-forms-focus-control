eb5d5a17f3989d817d6ddc1a37d2ad3514930c2b
Brian.Treese <brian.treese@socreate.it>
2026-01-25 10:11:29 -0800

    initial commit

---
+++ src/app/address-form/address-form.component.ts
@@ -0,0 +1,53 @@
+import { Component, signal } from '@angular/core';
+import { Field, form, pattern, required, submit } from '@angular/forms/signals';
+import { ValidationErrorsComponent } from '../shared/validation-errors/validation-errors.component';
+
+export interface Address {
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+}
+
+@Component({
+  selector: 'app-address-form',
+  templateUrl: './address-form.component.html',
+  styleUrls: ['./address-form.component.scss'],
+  imports: [Field, ValidationErrorsComponent],
+})
+export class AddressFormComponent {
+  readonly model = signal<Address>({
+    street: '',
+    city: '',
+    state: '',
+    zip: ''
+  });
+
+  readonly form = form(this.model, s => {
+    required(s.street, { message: 'Street is required' });
+    required(s.city, { message: 'City is required' });
+    required(s.state, { message: 'State is required' });
+    required(s.zip, { message: 'ZIP code is required' });
+    pattern(s.zip, /^\d{5}$/, { message: 'ZIP code must be 5 digits' });
+  });
+
+  protected reset() {
+    this.model.set({
+      street: '',
+      city: '',
+      state: '',
+      zip: ''
+    });
+  }
+
+  protected onSubmit(event: SubmitEvent) {
+    event.preventDefault();
+    submit(this.form, async data => {
+      console.log('Form submitted:', data().value());
+      // Return undefined if submission is successful
+      // Return validation errors if there are server-side errors
+      return undefined;
+    });
+  }
+}
+
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { ProfileFormComponent } from './sign-up/profile-form/profile-form.component';
+import { AddressFormComponent } from './address-form/address-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ProfileFormComponent],
-  template: `<app-profile-form />`
+  imports: [AddressFormComponent],
+  template: `<app-address-form />`
 })
 export class App {
 }
